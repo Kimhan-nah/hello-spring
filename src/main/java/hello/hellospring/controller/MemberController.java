3eaf9bf88710ba81d996761e4f4e7ea51fe334bd
Kimhannah <skqkdldhf98@naver.com>
2023-09-14 19:51:42 +0900

    :sparkles: feat: 회원 등록 기능 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,13 +1,35 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
   private final MemberService memberService;
 
+  // @Autowired로도 가능
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
   }
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm form) {
+    Member member = new Member();
+    member.setName(form.getName());
+    memberService.join(member);
+
+    return "redirect:/";
+  }
+
 }
